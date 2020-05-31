4c8755c7ccd3fe8fa5b649e9e8333927ecc1ec06
ken1flan <ken1flan@gmail.com>
2020-05-31 17:17:21 +0900

    Add validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,6 @@
 class Article < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 100 }
+  validates :body, presence: true, length: { maximum: 10000 }
+
+  enum status: { drafted: 0, published: 1 }
 end
