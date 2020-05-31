d1fa12b846efa04cf9deb5447de3d7ea9af46b75
ken1flan <ken1flan@gmail.com>
2020-05-31 17:03:50 +0900

    Add articles

---
+++ db/migrate/20200531065502_create_articles.rb
@@ -0,0 +1,11 @@
+class CreateArticles < ActiveRecord::Migration[6.0]
+  def change
+    create_table :articles do |t|
+      t.string :title, null: false
+      t.text :body, null: false
+      t.integer :status, null: false, default: 0
+
+      t.timestamps
+    end
+  end
+end
