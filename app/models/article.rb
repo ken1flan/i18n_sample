e53ae7d783e44437f5eb66af0a9122b9d24727ac
ken1flan <ken1flan@gmail.com>
2020-06-06 11:01:59 +0900

    Add validation to article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,14 @@
 class Article < ApplicationRecord
   validates :title, presence: true, length: { maximum: 100 }
   validates :body, presence: true, length: { maximum: 10000 }
+  validate :title_must_contain_cat
 
   enum status: { drafted: 0, published: 1 }
+
+  def title_must_contain_cat
+    return true if title.blank?
+    pattern = I18n.t('validation.article.title_must_contain_cat.condition').join('|')
+    regexp = Regexp.new(pattern)
+    errors.add(:title, :title_must_contain_cat) unless regexp.match?(title)
+  end
 end
